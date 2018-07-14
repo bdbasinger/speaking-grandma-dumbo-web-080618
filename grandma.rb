4ebe9638540996c3e8c35dc256eb6f3b8b6f8c3e
Brennan B <bdbasinger777@gmail.com>
2018-07-14 16:24:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,11 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(word)
+  puts "HUH?! SPEAK UP, SONNY!"
+  
+  
+  
+  
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
